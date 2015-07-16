193c804e7bfdfa1d288f9ac861dd0266aaaab7ff
Srevatsan Muralidharan <srevatsan_muralidharan@intuit.com>
2015-07-16 10:31:13 -0700

    Adding a hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+__author__ = 'smuralidharan2'
+
+
+# May be you can add two more of the exact same lines (replacing sri) in a feature branch that we can merge.
+
+def hello_world_sri():
+    print("Hello world")
+    return 0
+
